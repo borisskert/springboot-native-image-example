592f72f41222cf4c45738ffe81bad3ed40eec27d
Boris Skert <boris.skert@gmail.com>
2022-05-24 08:33:06 +0200

    Create project with Spring Initializr

---
+++ src/main/java/de/borisskert/springbootnativeimageexample/SpringbootNativeImageExampleApplication.java
@@ -0,0 +1,13 @@
+package de.borisskert.springbootnativeimageexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootNativeImageExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootNativeImageExampleApplication.class, args);
+	}
+
+}
